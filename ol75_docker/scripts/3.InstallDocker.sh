0cdf67e1f168d50563bb36b211d8df2b2317a237
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2018-09-03 16:03:21 +0200

    Reformatted comments.

--- ol75_docker/scripts/3.InstallDocker.sh
+++ ol75_docker/scripts/3.InstallDocker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname $0)
 #
-# Following https://oracle-base.com/articles/linux/docker-install-docker-on-oracle-linux-ol7
+# Install docker on Oracle Linux.
+# @author: Martien van den Akker, Darwin-IT Professionals.
 #
 function prop {
     grep "${1}" $SCRIPTPATH/makeDockerUser.properties|cut -d'=' -f2
@@ -29,11 +30,11 @@ echo 4. Check Docker install
 docker --version
 sudo systemctl start docker
 sudo systemctl status docker
+#
 echo 5. Change docker default folder
 # According to oracle-base you should create a filesystem, preferably using BTRFS, for the container-home. https://oracle-base.com/articles/linux/docker-install-docker-on-oracle-linux-ol7. 
 # But let's stick with ext4.
 ## Adapted from  https://sanenthusiast.com/change-default-image-container-location-docker/
-##https://stackoverflow.com/questions/30091681/why-does-docker-prompt-permission-denied-when-backing-up-the-data-volume
 echo 5.1. Find Storage Driver
 GREP_STRG_DRVR=$(sudo docker info |grep "Storage Driver")
 DOCKER_STORAGE_DRVR=${GREP_STRG_DRVR#*": "}
@@ -44,13 +45,14 @@ echo 5.3. Add reference to data folders for storage.
 DOCKER_DATA_HOME=/app/docker/data
 echo mkdir -p ${DOCKER_DATA_HOME}
 sudo mkdir ${DOCKER_DATA_HOME}
+#
+##https://stackoverflow.com/questions/30091681/why-does-docker-prompt-permission-denied-when-backing-up-the-data-volume
 echo disable selinux enforcing
 sudo setenforce 0
 #
 DOCKER_STORAGE_CFG=/etc/sysconfig/docker-storage
-DOCKER_CONTAINER_HOME=/data/docker
 sudo sh -c "echo 'DOCKER_STORAGE_OPTIONS = --graph=\"${DOCKER_DATA_HOME}\" --storage-driver=${DOCKER_STORAGE_DRVR}' >> ${DOCKER_STORAGE_CFG}"
-
+#
 echo 5.4 Reload deamon
 sudo systemctl daemon-reload 
 echo 5.5 Start docker again

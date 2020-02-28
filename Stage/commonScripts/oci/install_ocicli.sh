ce6ccd751b8e87a359252d8cd57090b4bdacce64
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2020-02-28 14:09:57 +0100

    install script voor oci cli.

---
+++ Stage/commonScripts/oci/install_ocicli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPTPATH=$(dirname $0)
+#
+OCICLI_MAIN_INSTALL=$SCRIPTPATH/install_ocicli_main.sh
+echo Install OCI cli with default settings
+#bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)"
+curl "https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh" >> $OCICLI_MAIN_INSTALL
+chmod +x $OCICLI_MAIN_INSTALL
+$OCICLI_MAIN_INSTALL --accept-all-defaults
+:

31709bcef8119645155353da36950db5e5ce6889
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2020-05-20 14:51:18 +0200

    Include check if nodemanager is started.

--- Stage/commonScripts/fmw/12.2.1.3/startStop/restartNmAdmSvc.sh
+++ Stage/commonScripts/fmw/12.2.1.3/startStop/restartNmAdmSvc.sh
@@ -12,3 +12,11 @@ SCRIPTPATH=$(dirname $0)
 echo
 echo Restart Nodemanager Admin Service
 sudo service nodemanager_admin restart
+echo
+echo Check if nodemanager is started...
+PS_ID=`ps -ef|grep weblogic.NodeManager |grep -v grep |awk 'END{print NR}'`
+
+if [ $PS_ID -gt 0 ]
+then echo "Nodemanager is RUNNING."
+else echo "Nodemanager is NOT RUNNING yet. Please retry"
+fi

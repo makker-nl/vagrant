d50f1b84e653e2bbbce90f1e78ec41b2b2fa2e5a
Martien van den Akker <makker.nl@gmail.com>
2020-09-23 13:24:15 +0200

    Prepare for CentOS 7.

--- Stage/commonScripts/redhat/CodeReadyStudio/installCodeReadyStudio.sh
+++ Stage/commonScripts/redhat/CodeReadyStudio/installCodeReadyStudio.sh
@@ -7,13 +7,15 @@ SCRIPTPATH=$(dirname $0)
 RHCRS_JAR_HOME=$STAGE_HOME/installBinaries/RedHat
 RHCRS_INSTALL_JAR=codereadystudio-12.16.0.GA-installer-standalone.jar
 RHCRS_HOME=$RH_BASE/codereadystudio
+# Set Java Home to Oracle JDK 8
+JAVA_HOME=/app/oracle/product/jdk8
 #
 echo "Checking CodeReady Home: "$RHCRS_HOME
 if [ ! -f "$RHCRS_HOME/codereadystudio" ]; then
  #
   # Install CodeReadyStudio
   echo Install CodeReadyStudio
-  java -jar $RHCRS_JAR_HOME/$RHCRS_INSTALL_JAR $SCRIPTPATH/InstallConfigRecord.xml
+  $JAVA_HOME/bin/java -jar $RHCRS_JAR_HOME/$RHCRS_INSTALL_JAR $SCRIPTPATH/InstallConfigRecord.xml
   echo Copy start script to user home
   mkdir -p ~/bin
   cp $SCRIPTPATH/codereadystudio.sh ~/bin

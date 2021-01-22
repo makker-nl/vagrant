8b5eb123676c02abb4b1400840ed7571bbc0bc28
Martien van den Akker <martien.van.den.akker@virtualsciences.nl>
2021-01-22 09:46:53 +0100

    Add AWS CLI Provisioner.

---
+++ Stage/commonScripts/amazon/install_awscli.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+SCRIPTPATH=$(dirname $0)
+#
+. $SCRIPTPATH/../install_env.sh
+#
+# https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
+#
+AWSCLI_ZIP=awscliv2.zip
+AWSCLI_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+AWSCLI_INSTALL_HOME=$INSTALL_TMP_DIR/AWS_CLI
+AWSCLI_INSTALL_ZIP=$AWSCLI_INSTALL_HOME/$AWSCLI_ZIP
+AWSCLI_INSTALL_ZIP_DIR=$AWSCLI_INSTALL_HOME/confluent-5.0.0
+AWSCLI_HOME=$OS_BASE/confluent
+AWSCLI_BIN=/usr/local/aws-cli/v2/current/dist/aws
+#
+echo "Install AWS CLI"
+echo "Checking AWS CLI binary: "$AWSCLI_BIN
+if [ ! -f "$AWSCLI_BIN" ]; then
+echo "Create folder: "$AWSCLI_INSTALL_HOME
+  mkdir -p $AWSCLI_INSTALL_HOME
+  echo Download $AWSCLI_URL to $AWSCLI_INSTALL_ZIP
+  curl $AWSCLI_URL -o $AWSCLI_INSTALL_ZIP
+  echo Unzip $AWSCLI_INSTALL_ZIP  to $AWSCLI_INSTALL_HOME
+  unzip -o $AWSCLI_INSTALL_ZIP -d $AWSCLI_INSTALL_HOME
+  echo Install aws cli using $AWSCLI_INSTALL_HOME/aws/install
+  sudo $AWSCLI_INSTALL_HOME/aws/install
+  echo Cleanup $INSTALL_TMP_DIR
+  rm -rf $INSTALL_TMP_DIR
+else
+  echo AWS CLI already installed.
+fi
+echo Installed AWS Version:
+aws --version
--- Stage/commonScripts/oracle/oci/install_ocicli.sh
+++ Stage/commonScripts/oracle/oci/install_ocicli.sh
@@ -7,4 +7,3 @@ echo Install OCI cli with default settings
 curl "https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh" >> $OCICLI_MAIN_INSTALL
 chmod +x $OCICLI_MAIN_INSTALL
 $OCICLI_MAIN_INSTALL --accept-all-defaults
-:

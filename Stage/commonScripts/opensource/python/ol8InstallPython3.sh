d7378b5549f5a0746a1e52f788e1e9d5f180184b
Martien van den Akker <martien.van.den.akker.oracle.com>
2023-05-03 16:01:00 +0200

    Set python3 alternative

--- Stage/commonScripts/opensource/python/ol8InstallPython3.sh
+++ Stage/commonScripts/opensource/python/ol8InstallPython3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname $0)
 #
-# Install helm.
-# @author: Martien van den Akker, VirtualSciences.
+# Install Python.
+# @author: Martien van den Akker, Oracle Technology Consulting.
 # 
-# Taken from: https://www.liquidweb.com/kb/how-to-install-python-3-on-centos-7/
+# Taken from: https://docs.oracle.com/en/operating-systems/oracle-linux/8/python/python-InstallingPython.html
 #
 PY3_BIN=/usr/bin/python3
 #
@@ -12,6 +12,8 @@ PY3_BIN=/usr/bin/python3
 sudo dnf update -y
 # 2: Install Python 3
 sudo dnf module install -y python39
-#
+# 3: Set alternative
+sudo alternatives --set python3 /usr/bin/python3.9
+# 4: Show version
 echo Get python3 version:
 python3 --version

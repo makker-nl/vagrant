87e1d601f58f01ddfb09d15842331325b1e0c2eb
Martien van den Akker <martien.van.den.akker.oracle.com>
2024-03-13 17:03:16 +0100

    Make script version independend

--- Stage/commonScripts/oracle/db/sqldev/installSqlDeveloper.sh
+++ Stage/commonScripts/oracle/db/sqldev/installSqlDeveloper.sh
@@ -10,23 +10,25 @@ export SQLDEV_HOME=$ORACLE_BASE/product/sqldeveloper
 SQLDEV_ZIP_HOME=$INSTALL_HOME/Oracle/DB/SQLDeveloper
 SQLDEV_INSTALL_HOME=$EXTRACT_HOME/Oracle/DB/SQLDeveloper
 #SQLDEV_INSTALL_ZIP=sqldeveloper-21.4.3.063.0100-no-jre.zip
-SQLDEV_INSTALL_ZIP=sqldeveloper-22.2.1.234.1810-no-jre.zip
-SQLDEV_INSTALL_FLDR=sqldeveloper
+#SQLDEV_INSTALL_ZIP=sqldeveloper-22.2.1.234.1810-no-jre.zip
+SQLDEV_BASE_NAME=sqldeveloper-*
+SQLDEV_INSTALL_ZIP=$(find $SQLDEV_ZIP_HOME -type f -name $SQLDEV_BASE_NAME | awk -F"/" '{print $NF}' | sort -r | head -1)
+#SQLDEV_INSTALL_FLDR=sqldeveloper
 #
 echo SQLDEV_HOME=$SQLDEV_HOME
 #
 if [ ! -d "$SQLDEV_HOME" ]; then
-  #Unzip SQLDEV_
-  if [ -f "$SQLDEV_ZIP_HOME/$SQLDEV_INSTALL_ZIP" ]; then    
+  if [[ -z "$SQLDEV_INSTALL_ZIP" ]]; then
+    echo There is no $SQLDEV_BASE_NAME*.zip file in $SQLDEV_ZIP_HOME.
+  else 
     mkdir -p $SQLDEV_INSTALL_HOME
-    echo Unzip $SQLDEV_ZIP_HOME/$SQLDEV_INSTALL_ZIP to $SQLDEV_HOME
+    echo Unzip $SQLDEV_ZIP_HOME/$SQLDEV_INSTALL_ZIP to $SQLDEV_INSTALL_HOME
     unzip $SQLDEV_ZIP_HOME/$SQLDEV_INSTALL_ZIP -d $SQLDEV_INSTALL_HOME
-    echo Move $SQLDEV_INSTALL_HOME/$SQLDEV_INSTALL_FLDR/* $SQLDEV_HOME
+    SQLDEV_INSTALL_NAME=$(find $SQLDEV_INSTALL_HOME/* -maxdepth 0 -type d  | awk -F"/" '{print $NF}' | sort -r | head -1)
+    echo Move $SQLDEV_INSTALL_HOME/$SQLDEV_INSTALL_NAME/* $SQLDEV_HOME
     mkdir -p $SQLDEV_HOME
-    mv $SQLDEV_INSTALL_HOME/$SQLDEV_INSTALL_FLDR/* $SQLDEV_HOME
-    rm -rf $SQLDEV_INSTALL_HOME/$SQLDEV_INSTALL_FLDR
-  else
-    echo $SQLDEV_ZIP_HOME/$SQLDEV_INSTALL_ZIP does not exist
+    mv $SQLDEV_INSTALL_HOME/$SQLDEV_INSTALL_NAME/* $SQLDEV_HOME
+    rm -rf $SQLDEV_INSTALL_HOME/$SQLDEV_INSTALL_NAME
   fi
 else
   echo sqldeveloper already unzipped

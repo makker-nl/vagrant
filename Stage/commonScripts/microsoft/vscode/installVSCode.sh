83e31cd6719c75208d14f7651f4a651c09898f03
Martien van den Akker <martien.van.den.akker.oracle.com>
2025-03-31 15:48:31 +0200

    Update installVSCode.sh

--- Stage/commonScripts/microsoft/vscode/installVSCode.sh
+++ Stage/commonScripts/microsoft/vscode/installVSCode.sh
@@ -4,7 +4,10 @@ SCRIPTPATH=$(dirname $0)
 # Install Visual Studio Code.
 # @author: Martien van den Akker, Oracle Consulting Technology Netherlands.
 # 
-# Taken from: https://blogs.oracle.com/wim/post/installing-visual-studio-code-on-oracle-linux-7
+# Taken from: https://code.visualstudio.com/docs/setup/linux
+# For VS Code Window title bar problem, due to: https://code.visualstudio.com/updates/v1_98#_custom-title-bar-on-linux
+#             https://stackoverflow.com/questions/53931328/vscode-through-mobaxterm-cannot-be-moved-resized-or-maximized.
+
 #
 VSCODE_BIN=/usr/bin/code
 SETTINGS_JSON=~/.config/Code/User/settings.json
